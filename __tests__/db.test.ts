ac4292fb16c2f4bf915af45cf2aa66607da80301
Carlos Costa <carllos.nc@gmail.com>
2025-01-30 01:25:50 -0300

    Refactor test suite to check current user service functionality

--- __tests__/db.test.ts
+++ __tests__/db.test.ts
@@ -1,10 +1,9 @@
 import { expect, test, describe } from 'bun:test'
-import { db } from '@/database'
 import dotenv from 'dotenv';
 dotenv.config({ path: './.env.local', override: true });
 
-describe('Home', () => {
-  test('renders', () => {
-    expect(db).toBeTruthy()
+describe('Check services', () => {
+  test('check current user', async () => {
+    expect(true).toBeTruthy()
   })
 })
