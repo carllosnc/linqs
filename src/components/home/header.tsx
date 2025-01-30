124c615acd0a1ffb7e5487e62345e3446f6e31fe
Carlos Costa <carllos.nc@gmail.com>
2025-01-30 01:23:28 -0300

    Refactor Header component to integrate UserButton and LogoHorizontal, simplifying user authentication UI

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -1,80 +1,16 @@
-import { Button } from "@/components/ui/button"
+import { UserButton } from "@/components/user-button"
+import { LogoHorizontal } from "@/components/logo"
 import { ModeToggle } from "@/components/mode-toggle"
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage
-} from "@/components/ui/avatar"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger
-}
-from "@/components/ui/dropdown-menu"
-import { auth, signOut } from "@/auth"
 
 export async function Header() {
-  const session = await auth()
-  const image = session?.user?.image
-  const name = session?.user?.name
-
-  function firstLetter(str: string) {
-    return str.charAt(0).toUpperCase()
-  }
-
-  function loginButton() {
-    if (session?.user) {
-      return (
-        <div className="flex items-center gap-[20px]">
-          <Button variant="outline">
-            <a href="/dashboard">Dashboard</a>
-          </Button>
-
-          <DropdownMenu>
-            <DropdownMenuTrigger>
-              <Avatar className="h-[32px] w-[32px]">
-                <AvatarImage src={image as string} />
-                <AvatarFallback>{ firstLetter(name!) }</AvatarFallback>
-              </Avatar>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuLabel> { name } </DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>
-                <a href="/dashboard">Dashboard</a>
-              </DropdownMenuItem>
-              <DropdownMenuItem className="text-red-500 w">
-                <form action={ async () => {
-                  "use server"
-                    await signOut()
-                  }}>
-                    <button className="text-red-600" type="submit"> Log out </button>
-                </form>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </div>
-      )
-    }
-
-    return (
-      <Button variant="outline">
-        <a href="/auth">Log in</a>
-      </Button>
-    )
-  }
-
   return (
-    <header className="w-full flex justify-center items-center h-[62px] px-6 border-b border-zinc-200 dark:border-zinc-800">
-      <div className="w-full max-w-[1200px] justify-between mx-auto h-full flex items-center">
-        <h1 className="text-zinc-800 dark:text-zinc-500 font-bold tracking-widest">ONE STACK</h1>
+    <header className="w-full flex justify-center items-center h-[62px] border-y border-color">
+      <div className="w-full max-w-[800px] border-x border-color md:border-none px-6 justify-between mx-auto h-full flex items-center">
+        <LogoHorizontal className="w-[100px] h-auto fill-black dark:fill-white" />
 
         <div className="flex gap-[10px]">
           <ModeToggle />
-          {loginButton()}
+          <UserButton />
         </div>
       </div>
     </header>
