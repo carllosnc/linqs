3b8c47211f67ba21ee125e6f62077f1045983723
Carlos Costa <carllos.nc@gmail.com>
2025-02-05 01:34:15 -0300

    Refactor Reset Password form layout for improved structure and readability

--- app/protected/reset-password/page.tsx
+++ app/protected/reset-password/page.tsx
@@ -9,28 +9,35 @@ export default async function ResetPassword(props: {
 }) {
   const searchParams = await props.searchParams;
   return (
-    <form className="flex flex-col w-full max-w-md p-4 gap-2 [&>input]:mb-4">
+    <form>
       <h1 className="text-2xl font-medium">Reset password</h1>
+
       <p className="text-sm text-foreground/60">
         Please enter your new password below.
       </p>
+
       <Label htmlFor="password">New password</Label>
+
       <Input
         type="password"
         name="password"
         placeholder="New password"
         required
       />
+
       <Label htmlFor="confirmPassword">Confirm password</Label>
+
       <Input
         type="password"
         name="confirmPassword"
         placeholder="Confirm password"
         required
       />
+
       <SubmitButton formAction={resetPasswordAction}>
         Reset password
       </SubmitButton>
+
       <FormMessage message={searchParams} />
     </form>
   );
