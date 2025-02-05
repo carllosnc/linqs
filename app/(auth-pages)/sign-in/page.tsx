0ebcbd0a4be03e7816f2d2ebd9626770ba38ca63
Carlos Costa <carllos.nc@gmail.com>
2025-02-05 01:32:45 -0300

    Refactor Sign In page to use new authentication components and enhance layout

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -3,42 +3,68 @@ import { FormMessage, Message } from "@/components/form-message";
 import { SubmitButton } from "@/components/submit-button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import {
+  AuthTitle,
+  AuthSubtitle,
+  AuthHeader,
+  AuthContent,
+  AuthCard,
+  AuthContainer
+} from "@/components/auth";
 import Link from "next/link";
 
 export default async function Login(props: { searchParams: Promise<Message> }) {
+
   const searchParams = await props.searchParams;
+
   return (
-    <form className="flex-1 flex flex-col min-w-64">
-      <h1 className="text-2xl font-medium">Sign in</h1>
-      <p className="text-sm text-foreground">
-        Don't have an account?{" "}
-        <Link className="text-foreground font-medium underline" href="/sign-up">
-          Sign up
-        </Link>
-      </p>
-      <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-        <Label htmlFor="email">Email</Label>
-        <Input name="email" placeholder="you@example.com" required />
-        <div className="flex justify-between items-center">
-          <Label htmlFor="password">Password</Label>
-          <Link
-            className="text-xs text-foreground underline"
-            href="/forgot-password"
-          >
-            Forgot Password?
-          </Link>
-        </div>
-        <Input
-          type="password"
-          name="password"
-          placeholder="Your password"
-          required
-        />
-        <SubmitButton pendingText="Signing In..." formAction={signInAction}>
-          Sign in
-        </SubmitButton>
-        <FormMessage message={searchParams} />
-      </div>
-    </form>
+    <AuthContainer>
+      <AuthCard>
+        <form className="flex-1 flex flex-col gap-[20px]">
+          <AuthHeader>
+            <AuthTitle> Sign in </AuthTitle>
+            <AuthSubtitle
+              text="Don't have an account?"
+              linkTitle="Sign up"
+              href="/sign-up"
+            />
+          </AuthHeader>
+
+          <AuthContent>
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input name="email" placeholder="you@example.com" required />
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                className="w-full min-w-full"
+                type="password"
+                name="password"
+                placeholder="Your password"
+                required
+              />
+            </div>
+
+            <div className="flex justify-between items-center">
+              <div>
+                <SubmitButton pendingText="Signing In..." formAction={signInAction}>
+                  Sign in
+                </SubmitButton>
+              </div>
+              <Link
+                className="link-color text-sm"
+                href="/forgot-password"
+              >
+                Forgot Password?
+              </Link>
+            </div>
+            <FormMessage message={searchParams} />
+          </AuthContent>
+
+        </form>
+      </AuthCard>
+    </AuthContainer>
   );
 }
