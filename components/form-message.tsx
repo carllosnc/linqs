4079c7af5aba44f73efa03fafad24d2e4263694c
Carlos Costa <carllos.nc@gmail.com>
2025-02-05 01:33:14 -0300

    Enhance FormMessage component styling for success, error, and informational messages

--- components/form-message.tsx
+++ components/form-message.tsx
@@ -4,20 +4,29 @@ export type Message =
   | { message: string };
 
 export function FormMessage({ message }: { message: Message }) {
+
+  console.log(message);
+
+  if (Object.keys(message).length === 0) return null;
+
   return (
     <div className="flex flex-col gap-2 w-full max-w-md text-sm">
       {"success" in message && (
-        <div className="text-foreground border-l-2 border-foreground px-4">
+        <div className="text-green-800 dark:text-green-300 bg-green-100 border-l-2 border-green-600 px-4 py-2 dark:bg-neutral-800">
           {message.success}
         </div>
       )}
+
       {"error" in message && (
-        <div className="text-destructive-foreground border-l-2 border-destructive-foreground px-4">
+        <div className="text-red-800 dark:text-red-300 bg-red-100 border-l-2 border-red-600 px-4 py-2 dark:bg-neutral-800">
           {message.error}
         </div>
       )}
+
       {"message" in message && (
-        <div className="text-foreground border-l-2 px-4">{message.message}</div>
+        <div className="text-blue-800 dark:text-blue-300 bg-blue-100 border-l-2 border-blue-600 px-4 py-2 dark:bg-neutral-800">
+          {message.message}
+        </div>
       )}
     </div>
   );
