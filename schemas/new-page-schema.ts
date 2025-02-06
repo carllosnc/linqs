6ceb0236783da555e3f0ede0efed3848589c1236
Carlos Costa <carllos.nc@gmail.com>
2025-02-06 01:27:12 -0300

    Update newPageSchema to limit description length to 100 characters

--- schemas/new-page-schema.ts
+++ schemas/new-page-schema.ts
@@ -6,5 +6,5 @@ export const newPageSchema = z.object({
     .max(100, { message: 'must be less than 100 characters' }),
   description: z.string()
     .min(10, { message: 'must be at least 10 characters' })
-    .max(500, { message: 'must be less than 500 characters' }),
+    .max(100, { message: 'must be less than 500 characters' }),
 });
