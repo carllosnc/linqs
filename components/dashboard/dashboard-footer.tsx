6de9b3ce91cbfc6edf6e3986856717744f3ca1b9
Carlos Costa <carllos.nc@gmail.com>
2025-02-06 01:26:31 -0300

    Refactor DashboardFooter to improve layout and styling

--- components/dashboard/dashboard-footer.tsx
+++ components/dashboard/dashboard-footer.tsx
@@ -1,7 +1,9 @@
 export function DashboardFooter() {
   return (
-    <footer className="text-center text-sm text-color p-4 border-t border-color">
-      Designed and coded by Carlos Costa
+    <footer className="text-center relative -top-[1px] text-sm text-color border-t border-color">
+      <div className="w-full border-x border-color bg-white max-w-[900px] dark:bg-neutral-900 p-4 m-auto">
+        Designed and coded by Carlos Costa
+      </div>
     </footer>
   )
 }
