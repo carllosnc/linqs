22e5269b1b56589d51d96143916ff9b83290f632
Carlos Costa <carllos.nc@gmail.com>
2025-02-06 01:26:34 -0300

    Refactor DashboardHeader for improved layout and readability

--- components/dashboard/dashboard-header.tsx
+++ components/dashboard/dashboard-header.tsx
@@ -6,10 +6,9 @@ export function DashboardHeader() {
   return (
     <header className="border-y border-color">
       <div className="flex bg-white dark:bg-neutral-900 gap-[10px] justify-between p-4 w-full max-w-[900px] border-x border-color m-auto">
-        <div className="flex gap-2 items-center">
-          <ThemeSwitcher />
-          <LogoHorizontal className="fill-black max-w-[100px] h-auto dark:fill-white" />
-        </div>
+        <ThemeSwitcher />
+
+        <LogoHorizontal className="fill-black max-w-[100px] h-auto dark:fill-white" />
 
         <div>
           <UserButton />
