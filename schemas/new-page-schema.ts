f7c01975ce24e4f538c8becbc4fbe4d72a29c37e
Carlos Costa <carllos.nc@gmail.com>
2025-02-09 19:22:14 -0300

    Update new page schema: make description optional and allow empty string

--- schemas/new-page-schema.ts
+++ schemas/new-page-schema.ts
@@ -6,5 +6,7 @@ export const newPageSchema = z.object({
     .max(80, { message: 'must be less than 80 characters' }),
   description: z.string()
     .min(10, { message: 'must be at least 10 characters' })
-    .max(80, { message: 'must be less than 80 characters' }),
+    .max(80, { message: 'must be less than 80 characters' })
+    .optional()
+    .or(z.literal('')),
 });
