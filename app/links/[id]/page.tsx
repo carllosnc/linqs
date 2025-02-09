23ed088ec65fe3ad8decebc55f7ee6b7e905d4d6
Carlos Costa <carllos.nc@gmail.com>
2025-02-09 19:20:31 -0300

    Refactor PublicPage component layout and improve description rendering

--- app/links/[id]/page.tsx
+++ app/links/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { Tables } from "@/types/database.types";
+import { Tables } from "@/database.types";
 import { createClient } from "@/utils/supabase/server";
 import { LogoSymbol, LogoHorizontal } from "@/components/logo"
 import { LinksLinkList } from "@/components/links/links-list";
@@ -31,24 +31,27 @@ export default async function PublicPage({ params }: { params: { id: string } })
   const linksData = linksRequest.data as Tables<'links'>[]
 
   return (
-    <main className="w-full bg-neutral-50 py-[30px] dark:bg-neutral-950 min-h-screen flex flex-col gap-[20px] items-center">
+    <main className="w-full bg-neutral-50 py-[30px] dark:bg-neutral-950 min-h-screen flex flex-col gap-[30px] items-center">
       <section className="flex flex-col gap-[20px] justify-between items-center w-full max-w-[400px]">
         <LogoSymbol className="fill-black max-w-[50px] h-auto dark:fill-white" />
         <div className="text-center flex flex-col gap-[2px]">
           <h1 className="text-[20px] title-color font-bold">
             {singlePageData.title}
           </h1>
-          <span className="text-color"> {singlePageData.descriptions} </span>
+          {
+            singlePageData.descriptions &&
+            <span className="text-color"> {singlePageData.descriptions} </span>
+          }
         </div>
       </section>
 
-      <hr className="w-full h-[1px] border-color" />
+      <hr className="w-full h-[1px] border-color border-dashed" />
 
       <div className="w-full">
         <LinksLinkList email="carllosnc@gmail.com" userId={userId as string} links={linksData} />
       </div>
 
-      <div className="px-[20px] mt-[60px] w-full">
+      <div className="px-[20px] w-full">
         <LogoHorizontal className="fill-neutral-300 m-auto w-full max-w-[200px] h-auto dark:fill-neutral-800" />
       </div>
     </main>
