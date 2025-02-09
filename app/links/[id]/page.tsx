e56509cc35d397b659dd05b3b516700ecd851144
Carlos Costa <carllos.nc@gmail.com>
2025-02-09 01:35:13 -0300

    Add redirect for non-existent pages and enhance layout with logo and link list

--- app/links/[id]/page.tsx
+++ app/links/[id]/page.tsx
@@ -1,5 +1,8 @@
 import { Tables } from "@/types/database.types";
 import { createClient } from "@/utils/supabase/server";
+import { LogoSymbol, LogoHorizontal } from "@/components/logo"
+import { LinksLinkList } from "@/components/links/links-list";
+import { redirect } from 'next/navigation'
 
 export default async function PublicPage({ params }: { params: { id: string } }) {
   const supabase = await createClient();
@@ -13,6 +16,12 @@ export default async function PublicPage({ params }: { params: { id: string } })
 
   const singlePageData = singlePageRequest.data as Tables<'pages'>
 
+  if (singlePageData === null) {
+    return redirect("/not-found");
+  }
+
+  const userId = singlePageData.user_id
+
   const linksRequest = await supabase
     .from("links")
     .select("*")
@@ -22,12 +31,26 @@ export default async function PublicPage({ params }: { params: { id: string } })
   const linksData = linksRequest.data as Tables<'links'>[]
 
   return (
-    <main className="w-full bg-neutral-50 dark:bg-neutral-950 min-h-screen flex flex-col justify-between">
-      <h1 className="text-2xl font-medium">
-        {singlePageData.title}
-        {/* check if page is public or not */}
-        {singlePageData.isPublic}
-      </h1>
+    <main className="w-full bg-neutral-50 py-[30px] dark:bg-neutral-950 min-h-screen flex flex-col gap-[20px] items-center">
+      <section className="flex flex-col gap-[20px] justify-between items-center w-full max-w-[400px]">
+        <LogoSymbol className="fill-black max-w-[50px] h-auto dark:fill-white" />
+        <div className="text-center flex flex-col gap-[2px]">
+          <h1 className="text-[20px] title-color font-bold">
+            {singlePageData.title}
+          </h1>
+          <span className="text-color"> {singlePageData.descriptions} </span>
+        </div>
+      </section>
+
+      <hr className="w-full h-[1px] border-color" />
+
+      <div className="w-full">
+        <LinksLinkList email="carllosnc@gmail.com" userId={userId as string} links={linksData} />
+      </div>
+
+      <div className="px-[20px] mt-[60px] w-full">
+        <LogoHorizontal className="fill-neutral-300 m-auto w-full max-w-[200px] h-auto dark:fill-neutral-800" />
+      </div>
     </main>
   );
 }
