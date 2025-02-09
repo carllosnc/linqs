ccb1ecff37ebb218d62af6e8a248d0bcccdd9d43
Carlos Costa <carllos.nc@gmail.com>
2025-02-09 19:21:59 -0300

    Update HomeHeader component: change "Sign in" and "Sign up" links to "Login"

--- components/home/home-header.tsx
+++ components/home/home-header.tsx
@@ -7,7 +7,6 @@ export function HomeHeader() {
   return (
     <header className="border-y border-color">
       <div className="flex gap-[10px] justify-between p-4 w-full max-w-[900px] border-x border-color m-auto">
-
         <div className="flex gap-2 items-center">
           <ThemeSwitcher />
           <LogoHorizontal className="fill-black max-w-[100px] h-auto dark:fill-white" />
@@ -15,10 +14,7 @@ export function HomeHeader() {
 
         <div className="flex gap-2">
           <Button size={"sm"} variant="outline">
-            <Link href="/sign-in">Sign in</Link>
-          </Button>
-          <Button size={"sm"} variant="default">
-            <Link href="/sign-up">Sign up</Link>
+            <Link href="/sign-in">Login</Link>
           </Button>
         </div>
       </div>
