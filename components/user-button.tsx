4d7202a23221b4fe1d0b11c9845823d77f69da0e
Carlos Costa <carllos.nc@gmail.com>
2025-02-09 19:21:18 -0300

    Enhance UserButton component with avatar display and user name

--- components/user-button.tsx
+++ components/user-button.tsx
@@ -12,20 +12,26 @@ import {
 import { User } from "lucide-react";
 import { createClient } from "@/utils/supabase/client";
 import { useRouter } from 'next/navigation'
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 
 export function UserButton() {
   const router = useRouter()
   const { user } = useGetUser()
 
+  console.log(user)
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
-        <div className="w-[40px] h-[40px] rounded-md border border-color flex items-center justify-center">
-          <User className="w-5 h-5 text-color" />
-        </div>
+        <Avatar>
+          <AvatarImage className="w-7 h-7 rounded-full" src={user?.user_metadata?.avatar_url} />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
-      <DropdownMenuLabel>{user?.email}</DropdownMenuLabel>
+        <DropdownMenuLabel>{user?.user_metadata?.full_name}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuLabel className="font-normal">{user?.email}</DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuItem onClick={async () => {
           await createClient().auth.signOut();
