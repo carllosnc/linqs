f39c6ac5773a46a4d303a805c3dbdae904a00d62
Carlos Costa <carllos.nc@gmail.com>
2025-02-09 01:35:20 -0300

    Add redirect for non-existent pages and replace DashboardNewLink with DashboardPageToolbar for improved layout

--- app/protected/page/[id]/page.tsx
+++ app/protected/page/[id]/page.tsx
@@ -1,13 +1,10 @@
 import { DashboardHeader } from "@/components/dashboard/dashboard-header";
 import { DashboardFooter } from "@/components/dashboard/dashboard-footer";
-import { DashboardNewLink } from "@/components/dashboard/dashboard-new-link";
+import { DashboardPageToolbar } from "@/components/dashboard/dashboard-page-toolbar";
 import { createClient } from "@/utils/supabase/server";
 import { DashboardListLinks } from "@/components/dashboard/dashboard-list-links";
 import { Tables } from "@/types/database.types";
-import { Button } from "@/components/ui/button";
-import { ChevronLeft, EllipsisVertical } from "lucide-react";
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default async function SinglePage({ params }: { params: { id: string } }) {
   const supabase = await createClient();
@@ -20,6 +17,10 @@ export default async function SinglePage({ params }: { params: { id: string } })
     .single();
 
   const singlePageData = singlePageRequest.data as Tables<'pages'>
+
+  if (singlePageData === null) {
+    return redirect("/not-found");
+  }
 
   const linksRequest = await supabase
     .from("links")
@@ -34,39 +35,12 @@ export default async function SinglePage({ params }: { params: { id: string } })
       <DashboardHeader />
 
       <section className="bg-white dark:bg-neutral-900 min-h-[87vh] border-x m-auto border-color w-full max-w-[900px]">
-        <div className="px-4 py-4 border-b border-color justify-between items-center flex gap-[10px]">
-          <div className="flex items-center gap-[20px]">
-            <Button variant="outline" size="icon">
-              <Link href="/protected">
-                <ChevronLeft />
-              </Link>
-            </Button>
-
-            <Sheet>
-              <SheetTrigger asChild>
-                <Button variant="outline">
-                  All pages
-                </Button>
-              </SheetTrigger>
-              <SheetContent side="left" className="w-full max-w-[400px]">
-                <SheetHeader>
-                  <SheetTitle>Are you absolutely sure?</SheetTitle>
-                  <SheetDescription>
-                    This action cannot be undone. This will permanently delete your account
-                    and remove your data from our servers.
-                  </SheetDescription>
-                </SheetHeader>
-              </SheetContent>
-            </Sheet>
-
-            <div className="flex flex-col">
-              <h2 className="title-color font-bold">{singlePageData.title}</h2>
-              <p className="text-sm text-color">{singlePageData.descriptions}</p>
-            </div>
-          </div>
+        <div className="px-4 py-4 border-b border-color justify-between items-center flex flex-col gap-[5px]">
+          <h2 className="title-color text-[18px] max-w-[500px] text-center font-semibold">{singlePageData.title}</h2>
+          <p className="text-sm text-color text-center max-w-[500px] truncate">{singlePageData.descriptions}</p>
         </div>
 
-        <DashboardNewLink page={singlePageData} pageId={id} />
+        <DashboardPageToolbar page={singlePageData} pageId={id} />
         <DashboardListLinks links={linksData} />
       </section>
 
