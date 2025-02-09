6e89793726d5cc2ae54aa2693e743f64f4e73c45
Carlos Costa <carllos.nc@gmail.com>
2025-02-09 19:21:13 -0300

    Add Google icon to GoogleButton component for improved UI

--- components/google-button.tsx
+++ components/google-button.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { createClient } from "@/utils/supabase/client";
+import { FcGoogle } from "react-icons/fc";
 
 export function GoogleButton() {
   function loginWithGoogle() {
@@ -16,7 +17,8 @@ export function GoogleButton() {
   }
 
   return (
-    <Button variant="outline" type="button" onClick={loginWithGoogle}>
+    <Button className="flex gap-[10px]" variant="outline" type="button" onClick={loginWithGoogle}>
+      <FcGoogle className="w-5 h-5" />
       Sign in with Google
     </Button>
   )
