4a050671d13a35e8b7dc699c58b3e573d9109918
Carlos Costa <carllos.nc@gmail.com>
2025-02-11 06:10:07 -0300

    Enhance SignIn page layout with updated background color and improved structure

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -20,12 +20,14 @@ export default function SignIn(props: any) {
   }, []);
 
   return (
-    <main className="w-full justify-center gap-[40px] bg-neutral-50 py-[30px] dark:bg-neutral-950 min-h-screen flex flex-col items-center">
+    <main className="w-full justify-center gap-[40px] bg-white py-[30px] dark:bg-neutral-950 min-h-screen flex flex-col items-center">
       <div className="flex flex-col gap-[10px] items-center">
         <LogoVertical className="fill-black max-w-[150px] h-auto dark:fill-white" />
         <h2 className="title-color text-color text-[18px]">Do login to continue.</h2>
       </div>
+
       <GoogleButton />
+
       <Link href="/" className="link-color">
         Back to home →
       </Link>
