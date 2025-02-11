c533b4a180e725d5d97126a71eb0afdfaa52a77d
Carlos Costa <carllos.nc@gmail.com>
2025-02-11 08:40:34 -0300

    Add HomeFooter component to Home page and adjust LogoSymbol size for better layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { HomeHeader } from "@/components/home/home-header";
+import { HomeFooter } from "@/components/home/home-footer"
 import { Illustration } from "@/components/illustration";
 import { LogoSymbol } from "@/components/logo";
 import { Button } from "@/components/ui/button";
@@ -8,10 +9,10 @@ export default async function Home() {
     <main>
       <HomeHeader />
 
-      <div className="w-full m-auto max-w-[900px] border-x border-color flex flex-col gap-[50px] py-[30px]">
+      <div className="w-full m-auto max-w-[900px] border-x border-color flex flex-col gap-[50px] pt-[30px]">
         <div className="w-full px-6 flex flex-col gap-[20px] items-center justify-center">
 
-          <LogoSymbol className="w-[120px] h-auto fill-black dark:fill-neutral-700" />
+          <LogoSymbol className="w-[80px] h-auto fill-black dark:fill-neutral-700" />
 
           <h1 className="text-[30px] md:text-[25px] text-neutral-400 dark:text-neutral-600 font-bold text-center">
             The better place to <span className="text-neutral-900 dark:text-neutral-300">organize</span> and <span className="text-neutral-900 dark:text-neutral-300">share</span> links
@@ -26,6 +27,8 @@ export default async function Home() {
 
         <Illustration className="w-full h-auto fill-neutral-200 dark:fill-neutral-800" />
       </div>
+
+      <HomeFooter />
     </main>
   );
 }
