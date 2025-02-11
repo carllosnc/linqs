33ffe85ffab4430b7314bc59a10faae26ae75ad8
Carlos Costa <carllos.nc@gmail.com>
2025-02-11 11:53:12 -0300

    Update HomeFooter component to remove left and right borders on larger screens for improved layout

--- components/home/home-footer.tsx
+++ components/home/home-footer.tsx
@@ -1,7 +1,7 @@
 export function HomeFooter() {
   return (
     <footer className="text-center relative -top-[1px] text-sm text-color border-t border-color">
-      <div className="w-full border-x border-color bg-white dark:bg-zinc-950 max-w-[900px] p-4 m-auto">
+      <div className="w-full border-x border-color lg:border-none bg-white dark:bg-zinc-950 max-w-[900px] p-4 m-auto">
         Designed and coded by Carlos Costa
       </div>
     </footer>
