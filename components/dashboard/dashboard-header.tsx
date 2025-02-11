7e5d5a764ba6aa35b2e6c27267a8a6c47ad247cd
Carlos Costa <carllos.nc@gmail.com>
2025-02-11 07:24:48 -0300

    Replace LogoSymbol with LogoHorizontal in DashboardHeader for improved branding visibility

--- components/dashboard/dashboard-header.tsx
+++ components/dashboard/dashboard-header.tsx
@@ -1,4 +1,4 @@
-import { LogoSymbol } from "@/components/logo";
+import { LogoHorizontal, LogoSymbol } from "@/components/logo";
 import { ThemeSwitcher } from "@/components/theme-switcher";
 import { UserButton } from "@/components/user-button";
 
@@ -8,7 +8,7 @@ export function DashboardHeader() {
       <div className="flex bg-white dark:bg-neutral-900 gap-[10px] justify-between items-center p-4 w-full max-w-[900px] border-x border-color m-auto">
         <ThemeSwitcher />
 
-        <LogoSymbol className="fill-black max-w-[36px] h-auto dark:fill-white" />
+        <LogoHorizontal className="fill-black max-w-[100px] h-auto dark:fill-white" />
 
         <UserButton />
       </div>
