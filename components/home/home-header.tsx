47ffefe38f8f62f90437267b07b6ef2f8baab8bb
Carlos Costa <carllos.nc@gmail.com>
2025-02-11 11:53:15 -0300

    Update HomeHeader component to remove borders on larger screens for improved layout

--- components/home/home-header.tsx
+++ components/home/home-header.tsx
@@ -6,7 +6,7 @@ import { ThemeSwitcher } from "@/components/theme-switcher";
 export function HomeHeader() {
   return (
     <header className="border-y border-color">
-      <div className="flex gap-[10px] justify-between p-4 w-full max-w-[900px] border-x border-color m-auto">
+      <div className="flex gap-[10px] justify-between p-4 w-full max-w-[900px] border-x border-color m-auto lg:border-none">
         <div className="flex gap-2 items-center">
           <ThemeSwitcher />
           <LogoHorizontal className="fill-black max-w-[100px] h-auto dark:fill-white" />
