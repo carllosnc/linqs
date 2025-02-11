6711a88cc5210f9edecc8d2616afcdfcc29980a9
Carlos Costa <carllos.nc@gmail.com>
2025-02-11 08:40:38 -0300

    Add HomeFooter component to enhance the layout of the Home page

---
+++ components/home/home-footer.tsx
@@ -0,0 +1,9 @@
+export function HomeFooter() {
+  return (
+    <footer className="text-center relative -top-[1px] text-sm text-color border-t border-color">
+      <div className="w-full border-x border-color bg-white dark:bg-zinc-950 max-w-[900px] p-4 m-auto">
+        Designed and coded by Carlos Costa
+      </div>
+    </footer>
+  )
+}
