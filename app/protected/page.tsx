745a4d4a9d2564bc9bb4eade6d55e9455af36fc5
Carlos Costa <carllos.nc@gmail.com>
2025-02-12 15:35:33 -0300

    Refactor ProtectedPage to use custom hook for data fetching and add loading state

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,35 +1,28 @@
-import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
-import { Tables } from "@/database.types";
+"use client"
+
 import { DashboardHeader } from "@/components/dashboard/dashboard-header";
 import { DashboardNewPage } from "@/components/dashboard/dashboard-new-page";
 import { DashboardListPages } from "@/components/dashboard/dashboard-list-pages";
 import { DashboardFooter } from "@/components/dashboard/dashboard-footer";
+import { DashboardLoading } from "@/components/dashboard/dashboard-loading";
+import { useGetPages } from "@/hooks/use-get-pages";
 
-export default async function ProtectedPage() {
-  const supabase = await createClient();
+export default function ProtectedPage() {
+  const { data, isLoading } = useGetPages()
 
-  const {data: { user }} = await supabase.auth.getUser();
-
-  if (!user) {
-    return redirect("/sign-in");
+  if (isLoading) {
+    return (
+      <DashboardLoading />
+    )
   }
-
-  const pagesRequest = await supabase
-    .from("pages")
-    .select("*")
-    .eq("user_id", user?.id)
-    .order("created_at", { ascending: false });
-
-  const pages = pagesRequest.data as Tables<'pages'>[]
 
   return (
     <main className="w-full bg-neutral-50 dark:bg-neutral-950 min-h-screen flex flex-col justify-between">
       <DashboardHeader />
 
       <section className="bg-white dark:bg-neutral-900 min-h-[87vh] border-x m-auto border-color w-full max-w-[900px]">
-        <DashboardNewPage pages={pages} userId={user?.id} />
-        <DashboardListPages pages={pages} />
+        <DashboardNewPage pages={data?.pages!} userId={data?.user?.id!} />
+        <DashboardListPages pages={data?.pages!} />
       </section>
 
       <DashboardFooter />
