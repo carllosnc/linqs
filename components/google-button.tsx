a270d0993e2a91263ac93876474aa74c8a29b85e
Carlos Costa <carllos.nc@gmail.com>
2025-02-11 22:25:22 -0300

    Refactor GoogleButton component to use dynamic redirect URL for OAuth login

--- components/google-button.tsx
+++ components/google-button.tsx
@@ -5,25 +5,12 @@ import { createClient } from "@/utils/supabase/client";
 import { FcGoogle } from "react-icons/fc";
 
 export function GoogleButton() {
-
-  const getURL = () => {
-    let url =
-      process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-      process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-      'http://localhost:3000/'
-    // Make sure to include `https://` when not localhost.
-    url = url.startsWith('http') ? url : `https://${url}`
-    // Make sure to include a trailing `/`.
-    url = url.endsWith('/') ? url : `${url}/`
-    return url
-  }
-
   function loginWithGoogle() {
     const supabase = createClient()
 
     supabase.auth.signInWithOAuth({
       options: {
-        redirectTo: `https://linqs.vercel.app/auth/callback`,
+        redirectTo: `${window.location.origin}/auth/callback`,
       },
       provider: 'google',
     })
