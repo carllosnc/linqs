5f3b757a19aab1ba5438daab8f7b209b9068ba6e
Carlos Costa <carllos.nc@gmail.com>
2025-02-14 18:46:45 -0300

    Update queryKey in useGetMetaData hook for clarity

--- hooks/use-get-meta-data.ts
+++ hooks/use-get-meta-data.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 
 export function useGetMetaData(url: string) {
   const query = useQuery({
-    queryKey: ["meta-data"],
+    queryKey: ["useGetMetaData"],
     queryFn: async () => {
       const response = await fetch(`https://api.dub.co/metatags?url=${url}`);
       const data = await response.json();
