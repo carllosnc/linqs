f5d2e7946080488690c3e383b5ce03eff0fb62b1
Carlos Costa <carllos.nc@gmail.com>
2025-02-16 03:22:55 -0300

    Add background class to main and update title styling in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
+    <main className="page-bg">
       <HomeHeader />
 
       <div className="w-full m-auto max-w-[900px] border-x border-color flex flex-col gap-[50px] pt-[30px] lg:border-none">
@@ -17,8 +17,8 @@ export default function Home() {
 
           <LogoSymbol className="w-[80px] h-auto fill-black dark:fill-neutral-700" />
 
-          <h1 className="text-[30px] md:text-[25px] text-neutral-400 dark:text-neutral-600 font-bold text-center">
-            The better place to <span className="text-neutral-900 dark:text-neutral-300">organize</span> and <span className="text-neutral-900 dark:text-neutral-300">share</span> links
+          <h1 className="text-[30px] w-full max-w-[500px] md:text-[25px] title-color font-bold text-center">
+            The better place to organize and share favorite links
           </h1>
 
           <p className="text-center text-neutral-500 w-full text-lg md:text-md max-w-[500px]">
@@ -30,7 +30,7 @@ export default function Home() {
           </Link>
         </div>
 
-        <Illustration className="w-full h-auto fill-neutral-200 dark:fill-neutral-800" />
+          <Illustration className="w-full h-auto fill-neutral-300 dark:fill-neutral-800" />
       </div>
 
       <HomeFooter />
