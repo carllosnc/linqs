0550a2ed5bd32e1e4783a1985e1f7461fa6bebcd
Carlos Costa <carllos.nc@gmail.com>
2025-02-18 00:24:22 -0300

    Refactor ProtectedPage to simplify structure and enhance metadata

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,32 +1,11 @@
-"use client"
+import DashboardContent from "@/components/dashboard/dashboard-content"
 
-import { DashboardHeader } from "@/components/dashboard/dashboard-header";
-import { DashboardNewPage } from "@/components/dashboard/dashboard-new-page";
-import { DashboardListPages } from "@/components/dashboard/dashboard-list-pages";
-import { DashboardFooter } from "@/components/dashboard/dashboard-footer";
-import { DashboardLoading } from "@/components/dashboard/dashboard-loading";
-import { usePageCrud } from "@/hooks/use-page-crud";
+export default async function ProtectedPage() {
+ return (
+  <DashboardContent />
+ )
+}
 
-export default function ProtectedPage() {
-  const { data, isLoading } = usePageCrud().getAllPages;
-  const globalPages = usePageCrud().globalPages;
-
-  if (isLoading) {
-    return (
-      <DashboardLoading />
-    )
-  }
-
-  return (
-    <main className="w-full page-bg min-h-screen flex flex-col justify-between">
-      <DashboardHeader />
-
-      <section className="p-4 min-h-[87vh] border-x m-auto border-color w-full max-w-[900px] flex flex-col gap-[15px]">
-        <DashboardNewPage pages={globalPages} userId={data?.user?.id!} />
-        <DashboardListPages pages={globalPages} />
-      </section>
-
-      <DashboardFooter />
-    </main>
-  );
+export const metadata = {
+  title: "Linqs • Dashboard",
 }
