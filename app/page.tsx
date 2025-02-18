02d53a503b68d5f4d5b1339c7ed60fec12004fd9
Carlos Costa <carllos.nc@gmail.com>
2025-02-17 21:02:48 -0300

    Refactor Home component for improved layout and styling; replace Link with GoogleButton for enhanced user experience

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,14 @@ import { HomeHeader } from "@/components/home/home-header";
 import { HomeFooter } from "@/components/home/home-footer"
 import { Illustration } from "@/components/illustration";
 import { LogoSymbol } from "@/components/logo";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
 import { SiNextdotjs, SiTypescript, SiReact, SiSupabase } from "react-icons/si";
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from "@/components/ui/tooltip";
+import { GoogleButton } from "@/components/google-button"
 
 export default function Home() {
   return (
@@ -17,25 +21,25 @@ export default function Home() {
       <div className="w-full m-auto max-w-[900px] border-x border-color flex flex-col gap-[50px] pt-[30px] lg:border-none">
         <div className="w-full px-6 flex flex-col gap-[20px] items-center justify-center">
 
-          <LogoSymbol className="w-[80px] h-auto fill-black dark:fill-neutral-700" />
+          <LogoSymbol className="w-[80px] h-auto fill-black dark:fill-white" />
 
-          <h1 className="text-[30px] w-full max-w-[500px] md:text-[25px] title-color font-bold text-center">
-            The better place to organize and share favorite links
+          <h1 className="text-[90px] leading-[90px] w-full max-w-[500px] md:text-[70px] md:leading-[70px] font-black text-center">
+            <span>Save</span> <br />
+            <span>Manage</span> <br />
+            <span>Share</span> <br />
           </h1>
 
-          <p className="text-center text-neutral-500 w-full text-[20px] md:text-md max-w-[600px]">
+          <p className="text-center text-color w-full text-[20px] md:text-md max-w-[600px]">
             Linqs is a sleek, intuitive platform designed to help you effortlessly organize, manage, and share your favorite links.
           </p>
 
-          <Link prefetch={false} href="/sign-in">
-            <Button> Get Started </Button>
-          </Link>
+          <GoogleButton />
         </div>
 
         <hr className="border-color" />
 
         <div className="p-6 flex flex-col w-full items-center justify-center gap-[20px]">
-          <p className="text-center text-[20px] max-w-[600px] text-neutral-500">
+          <p className="text-center text-[20px] max-w-[600px] text-color">
             This is a personal project that I'm developing for free. The main goal is to practice my frontend skills and solve some problems that I have in my daily life with my bookmarks.
           </p>
 
@@ -87,7 +91,7 @@ export default function Home() {
 
         <hr className="border-color" />
 
-          <Illustration className="w-full h-auto fill-neutral-300 dark:fill-neutral-800" />
+        <Illustration className="w-full h-auto fill-neutral-300 dark:fill-neutral-800" />
       </div>
 
       <HomeFooter />
