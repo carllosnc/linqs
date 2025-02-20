884de9d72c1e1a4fd8211180deb7f127cb9165ea
Carlos Costa <carllos.nc@gmail.com>
2025-02-19 23:26:06 -0300

    Refactor LinksLoading component to use Skeleton for improved loading state display

--- components/links/links-loading.tsx
+++ components/links/links-loading.tsx
@@ -1,9 +1,17 @@
-import { Spinner } from "@/components/ui/spinner";
+import { Skeleton } from "../ui/skeleton";
 
 export function LinksLoading() {
   return (
-    <main className="w-full bg-neutral-50 dark:bg-neutral-950 min-h-screen flex flex-col justify-center items-center">
-        <Spinner />
+    <main className="w-full px-[20px] py-[20px] page-bg min-h-screen flex flex-col justify-center items-center gap-[30px]">
+      <div className="w-full max-w-[550px] flex flex-col gap-[20px] m-auto">
+        <Skeleton className="card-block bg-white h-[30px] max-w-[400px]" />
+        <Skeleton className="card-block bg-white h-[30px] w-full" />
+        <Skeleton className="card-block bg-white h-[200px] w-full" />
+        <Skeleton className="card-block bg-white h-[200px] w-full" />
+        <Skeleton className="card-block bg-white h-[200px] w-full" />
+        <Skeleton className="card-block bg-white h-[200px] w-full" />
+        <Skeleton className="card-block bg-white h-[200px] w-full" />
+      </div>
     </main>
   )
 }
