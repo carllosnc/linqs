31511984b5e4cdb891a35d7f9b5c3abbb023a96f
Carlos Costa <carllos.nc@gmail.com>
2025-02-19 23:25:59 -0300

    Refactor DashboardHeader layout for improved responsiveness on larger screens

--- components/dashboard/dashboard-header.tsx
+++ components/dashboard/dashboard-header.tsx
@@ -7,7 +7,7 @@ import { UserButton } from "@/components/user-button";
 export function DashboardHeader() {
   return (
     <header className="border-b border-color">
-      <div className="flex gap-[10px] justify-between items-center p-4 w-full max-w-[900px] border-x border-color m-auto">
+      <div className="flex gap-[10px] lg:border-none justify-between items-center p-4 w-full max-w-[900px] border-x border-color m-auto">
         <ThemeSwitcher />
 
         <LogoHorizontal className="fill-black max-w-[100px] h-auto dark:fill-white" />
