6d78ea78998b0da1731b73b94a6775a307e78e7f
Carlos Costa <carllos.nc@gmail.com>
2025-02-21 13:05:51 -0300

    Refactor SinglePage component to generate metadata dynamically from Supabase

--- app/protected/page/[id]/page.tsx
+++ app/protected/page/[id]/page.tsx
@@ -1,10 +1,30 @@
-"use client"
-
 import SinglePageContent from "@/components/single-page/single-page-content";
-import { useParams } from "next/navigation";
+import { Tables } from "@/database.types";
+import { createClient } from "@/utils/supabase/client";
 
-export default function SinglePage() {
-  const params = useParams<{ id: string }>()
+export async function generateMetadata(
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const supabase = createClient();
+  const { id } = await params
 
-  return <SinglePageContent id={params.id} />
+  const singlePageRequest = await supabase
+    .from("pages")
+    .select("*")
+    .eq("id", id)
+    .single();
+
+  const data = singlePageRequest.data as Tables<'pages'>
+
+  return {
+    title: data.title,
+  }
+}
+
+export default async function SinglePage(
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params
+
+  return <SinglePageContent id={id} />
 }
