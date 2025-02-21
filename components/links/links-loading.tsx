f35341158a422941810ae420a34aa12424a89d95
Carlos Costa <carllos.nc@gmail.com>
2025-02-21 13:05:59 -0300

    Replace Skeleton loading indicators with Spinner component in LinksLoading

--- components/links/links-loading.tsx
+++ components/links/links-loading.tsx
@@ -1,17 +1,9 @@
-import { Skeleton } from "../ui/skeleton";
+import { Spinner } from "@/components/ui/spinner";
 
 export function LinksLoading() {
   return (
     <main className="w-full px-[20px] py-[20px] page-bg min-h-screen flex flex-col justify-center items-center gap-[30px]">
-      <div className="w-full max-w-[550px] flex flex-col gap-[20px] m-auto">
-        <Skeleton className="card-block bg-white h-[30px] max-w-[400px]" />
-        <Skeleton className="card-block bg-white h-[30px] w-full" />
-        <Skeleton className="card-block bg-white h-[200px] w-full" />
-        <Skeleton className="card-block bg-white h-[200px] w-full" />
-        <Skeleton className="card-block bg-white h-[200px] w-full" />
-        <Skeleton className="card-block bg-white h-[200px] w-full" />
-        <Skeleton className="card-block bg-white h-[200px] w-full" />
-      </div>
+      <Spinner />
     </main>
   )
 }
